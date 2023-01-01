a4df3adc381ad41d3c71c80e8da9e22ce14303e6
DJ <djpric91@gmail.com>
2023-01-01 14:13:12 -0600

    Lock file plus circle move in diagonal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+use ggez::event;
+use ggez::graphics::{self, Color};
+use ggez::{Context, GameResult};
+use ggez::glam::*;
+
+struct MainState {
+    pos_x: f32,
+    pos_y: f32,
+}
+
+impl MainState {
+    fn new() -> GameResult<MainState> {
+        let s = MainState { pos_x: 0.0, pos_y: 0.0 };
+        Ok(s)
+    }
+}
+
+impl event::EventHandler<ggez::GameError> for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        self.pos_x = self.pos_x % 800.0 + 1.0;
+        self.pos_y = self.pos_x;
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        let mut canvas = graphics::Canvas::from_frame(
+            ctx,
+            graphics::Color::from([0.9, 0.1, 0.3, 1.0]),
+        );
+
+        let circle = graphics::Mesh::new_circle(
+            ctx,
+            graphics::DrawMode::fill(),
+            Vec2::new(0.0, 0.0),
+            100.0,
+            2.0,
+            Color::BLACK,
+        )?;
+        canvas.draw(&circle, Vec2::new(self.pos_x, self.pos_y));
+
+        canvas.finish(ctx)?;
+        Ok(())
+    }
+}
+
+pub fn main() -> GameResult {
+    let cb = ggez::ContextBuilder::new("super_simple", "ggez");
+    let (ctx, event_loop) = cb.build()?;
+
+    let state = MainState::new()?;
+    event::run(ctx, event_loop, state)
 }
